0b72f7051882a52e25a7c62e06d8625989869470
Gabrielparizet <parizetgabriel@gmail.com>
2023-05-17 17:13:18 +0200

    Building address details screen

--- screens/AddressDetails.js
+++ screens/AddressDetails.js
@@ -1,15 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
 
-export default function AddressDetails({ route }) {
+export default function AddressDetails({ route, profile }) {
   const { selectedAddressDetails } = route.params;
   console.log(selectedAddressDetails);
 
   return (
     <>
-      <View>
-        <Text>Welcome to the address page</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.detailsContainer}>
+          <Image
+            source={require("../assets/images/le_chateaubriand.jpeg")}
+            style={styles.image}
+          />
+          <Text style={styles.textContainer}>
+            {selectedAddressDetails.name}
+          </Text>
+          <Text style={styles.textContainer}>
+            {selectedAddressDetails.postal_address}
+          </Text>
+        </View>
+      </ScrollView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#425F57",
+  },
+  image: {
+    width: "100%",
+    // height: 40,
+  },
+  detailsContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+  },
+  textContainer: {
+    color: "blue",
+  },
+});
